e7ae462f3d662cf74e9314fd2aa6a7022b306418
eslansa <eslansanchez12345@gmail.com>
2023-09-04 23:13:10 -0400

    cambiando el formato de las fotos

--- components/TopLeftImg.js
+++ components/TopLeftImg.js
@@ -3,8 +3,8 @@ import Image from 'next/image'
 
 const TopLeftImg = () => {
   return <div className='absolute top-0 left-0 mix-blend-color-dodge z-10 opacity-40 bg-image hidden sm:flex'>
-    <Image src="/top-left-img.png" width={400} height={400} priority={true} alt='' />
+    <Image src="/top-left-img.webp" width={400} height={400} priority={true} alt='' />
   </div>;
-};
+}
 
 export default TopLeftImg;

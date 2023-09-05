cea6f8fcf1670871fa867cb9a8bd2a414fa3861e
eslansa <eslansanchez12345@gmail.com>
2023-09-05 01:30:52 -0400

    al fin arregle lo de la img xd

--- components/TopLeftImg.js
+++ components/TopLeftImg.js
@@ -2,9 +2,11 @@
 import Image from 'next/image'
 
 const TopLeftImg = () => {
-  return <div className='absolute top-0 left-0 mix-blend-color-dodge z-10 opacity-40 bg-image hidden sm:flex'>
-    <Image src="/top-left-img.webp" width={400} height={400} priority={true} alt='' />
-  </div>;
-}
+  return (
+  <div className='absolute top-0 left-0 mix-blend-color-dodge z-10 opacity-40 bg-image hidden sm:flex'>
+    <Image src="/top-left-img.webp" width={400} height={400} priority={true} alt='' className="w-full h-full" />
+  </div>
+  );
+};
 
 export default TopLeftImg;

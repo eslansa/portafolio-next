7583723b7c6bef1d41bc6e9918ea86f8613b56dc
eslansa <eslansanchez12345@gmail.com>
2023-10-16 14:37:09 -0400

    changes SEO

--- components/Layout.js
+++ components/Layout.js
@@ -1,8 +1,9 @@
-import Head from 'next/head'
-import { Sora } from '@next/font/google'
-import Nav from '../components/Nav'
-import Header from '../components/Header'
-import TopLeftImg from '../components/TopLeftImg'
+// Importa los estilos de Tailwind directamente
+import 'tailwindcss/tailwind.css';
+import { Sora } from '@next/font/google';
+import Nav from '../components/Nav';
+import Header from '../components/Header';
+import TopLeftImg from '../components/TopLeftImg';
 
 const sora = Sora({
   subsets: ['latin'],
@@ -10,37 +11,19 @@ const sora = Sora({
   weight: ['100', '200', '300', '400', '500', '600', '700', '800']
 })
 
+// Definir estilos de fuente con Tailwind
+const fontStyles = 'font-sora';
+
 const Layout = ({ children }) => {
-  const structuredData = {
-    "@context": "http://schema.org",
-    "@type": "WebSite",
-    "name": "Nombre de tu sitio web",
-    "url": "URL de tu sitio web",
-    "potentialAction": {
-      "@type": "SearchAction",
-      "target": "URL de búsqueda en tu sitio web",
-      "query-input": "required name=search_term_string"
-    }
-  }
 
   return (
-    <div className={`page bg-site text-white bg-cover bg-no-repeat ${sora.variable} font-sora relative`}>
-      <Head>
-        <title>Eslan Sanchez Alvarez</title>
-        <meta name="Eslan Sanchez Alvarez" content="Portafolio Web de Eslan Sanchez" />
-        <meta property="og:Eslan Sanchez" content="eslansa" />
-        <meta property="og:Eslan" content="eslansa" />
-        <meta property="og:eslansa" content="eslansa" />
-        <script type="application/ld+json">
-          {JSON.stringify(structuredData)}
-        </script>
-      </Head>
+    <div className={`page bg-site text-white bg-cover bg-no-repeat ${fontStyles} relative`}>
       <Header />
       <TopLeftImg />
       <Nav />
       {children}
     </div>
-  )
-}
+  );
+};
 
-export default Layout
+export default Layout;
--- pages/_app.js
+++ pages/_app.js
@@ -9,11 +9,42 @@ import { useRouter } from 'next/router'
 
 // framer motion
 import { AnimatePresence, motion } from 'framer-motion'
+import Head from 'next/head'
 
 function MyApp ({ Component, pageProps }) {
   const router = useRouter()
+  const structuredData = {
+    "@context": "http://schema.org",
+    "@type": "Person",
+    "name": "Eslan Sanchez Alvarez",
+    "url": "https://eslansa.pages.dev",
+    "sameAs": [
+      "https://github.com/eslansa",
+      "https://twitter.com/lil_eslan",
+      // Otras redes sociales o plataformas
+    ],
+    "jobTitle": "Desarrollador Web",
+    "worksFor": {
+      "@type": "Organization",
+      "name": "Eslan Sanchez"
+    }
+  };
+  
   return (
     <Layout>
+            <Head>
+        <title>Eslan Sanchez Alvarez</title>
+        <meta name="description" content="¡Hola! Soy Eslan Sanchez Alvarez, un apasionado desarrollador web. Explora mi portafolio para conocer mi trabajo y habilidades." />
+        {/* Etiquetas Open Graph para compartir en redes sociales */}
+        <meta property="og:title" content="Eslan Sanchez Alvarez" />
+        <meta property="og:description" content="¡Hola! Soy Eslan Sanchez Alvarez, un apasionado desarrollador web. Explora mi portafolio para conocer mi trabajo y habilidades." />
+        <meta property="og:url" content="https://eslansa.pages.dev" />
+        <meta property="og:image" content="" />
+        {/* JSON-LD para datos estructurados */}
+        <script type="application/ld+json">
+          {JSON.stringify(structuredData)}
+        </script>
+      </Head>
       <AnimatePresence mode='wait'>
         <motion.div key={router.route} className='h-full'>
           <Transition />

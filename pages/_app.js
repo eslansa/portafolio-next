eecb8b7b1b320429cb54fa07f07b7de4f16dec86
eslansa <eslansanchez12345@gmail.com>
2023-10-23 18:42:07 -0400

    fix-seo

--- pages/_app.js
+++ pages/_app.js
@@ -1,50 +1,39 @@
 import '../styles/globals.css'
-
-// components
 import Layout from '../components/Layout'
 import Transition from '../components/Transition'
-
-// router
 import { useRouter } from 'next/router'
-
-// framer motion
 import { AnimatePresence, motion } from 'framer-motion'
-import Head from 'next/head'
+import { NextSeo } from 'next-seo'
 
 function MyApp ({ Component, pageProps }) {
   const router = useRouter()
-  const structuredData = {
-    "@context": "http://schema.org",
-    "@type": "Person",
-    "name": "Eslan Sanchez Alvarez",
-    "url": "https://eslansa.pages.dev",
-    "sameAs": [
-      "https://github.com/eslansa",
-      "https://twitter.com/lil_eslan",
-      // Otras redes sociales o plataformas
-    ],
-    "jobTitle": "Desarrollador Web",
-    "worksFor": {
-      "@type": "Organization",
-      "name": "Eslan Sanchez"
-    }
-  };
-  
+
   return (
     <Layout>
-            <Head>
-        <title>Eslan Sanchez Alvarez</title>
-        <meta name="description" content="¡Hola! Soy Eslan Sanchez Alvarez, un apasionado desarrollador web. Explora mi portafolio para conocer mi trabajo y habilidades." />
-        {/* Etiquetas Open Graph para compartir en redes sociales */}
-        <meta property="og:title" content="Eslan Sanchez Alvarez" />
-        <meta property="og:description" content="¡Hola! Soy Eslan Sanchez Alvarez, un apasionado desarrollador web. Explora mi portafolio para conocer mi trabajo y habilidades." />
-        <meta property="og:url" content="https://eslansa.pages.dev" />
-        <meta property="og:image" content="" />
-        {/* JSON-LD para datos estructurados */}
-        <script type="application/ld+json">
-          {JSON.stringify(structuredData)}
-        </script>
-      </Head>
+      <NextSeo
+        title="Eslan Sanchez Alvarez"
+        description="¡Hola! Soy Eslan Sanchez Alvarez, un apasionado desarrollador web. Explora mi portafolio para conocer mi trabajo y habilidades."
+        canonical="https://eslansa.pages.dev"
+        openGraph={{
+          url: 'https://eslansa.pages.dev',
+          title: 'Eslan Sanchez Alvarez',
+          description: '¡Hola! Soy Eslan Sanchez Alvarez, un apasionado desarrollador web. Explora mi portafolio para conocer mi trabajo y habilidades.',
+          // images: [
+          //   {
+          //     url: 'https://example.com/image.jpg', // Reemplaza esto con la URL de tu imagen
+          //     width: 800,
+          //     height: 600,
+          //     alt: 'Descripción de la imagen', // Reemplaza esto con la descripción de tu imagen
+          //   },
+          // ],
+        }
+      }
+        twitter={{
+          handle: '@lil_eslan',
+          site: '@lil_eslan',
+          cardType: 'summary_large_image',
+        }}
+      />
       <AnimatePresence mode='wait'>
         <motion.div key={router.route} className='h-full'>
           <Transition />

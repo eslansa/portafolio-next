7a4c5ecae353bf33da7e7ddbb54dfe4d03e9a595
eslansa <eslansanchez12345@gmail.com>
2023-10-30 09:11:23 -0400

    prueba

--- components/Explosion.js
+++ components/Explosion.js
@@ -4,7 +4,7 @@ import Image from 'next/image'
 const Explosion = () => {
   return (
     <div className='bg-none xl:bg-cover xl:bg-right xl:bg-no-repeat w-full h-full absolute mix-blend-color-dodge translate-z-0'>
-      <Image src='/bg-explosion.webp' width={700} height={600} className='w-full h-full' alt='' />
+      <Image src='/bg-explosion.webp' width={700} height={600} className='w-full h-full' alt='Background explosion' />
     </div>
   )
 }

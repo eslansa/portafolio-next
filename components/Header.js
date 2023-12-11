e166062d9627f5ff8b69b5e38161935d24e937d2
eslansa <eslansanchez12345@gmail.com>
2023-12-11 16:15:47 -0500

    fixed_problems

--- components/Header.js
+++ components/Header.js
@@ -3,11 +3,11 @@ import Link from 'next/link'
 
 // components
 import Socials from '../components/Socials'
+import Head from 'next/head'
 
 const Header = () => {
   return (
-    <header className='absolute z-30 w-full flex items-center px-16 xl:px-0 xl:h-[90px]'>
-      <meta name='google-site-verification' content='7pY3HSqGDEy_LyhtYnPQmWomQGWQkC740Gmb9Qk6_FI' />
+    <div className='absolute z-30 w-full flex items-center px-16 xl:px-0 xl:h-[90px]'>
       <script type='text/javascript' id='pagemapIdscript' defer src='https://bot.linkbot.com/bot.js#19613-7fb33?property-id=19613-7fb33' />
       <div className=' container mx-auto'>
         <div className='flex flex-col lg:flex-row justify-between items-center gap-y-0 py-5'>
@@ -19,7 +19,7 @@ const Header = () => {
           <Socials />
         </div>
       </div>
-    </header>
+    </div>
   )
 }
 
--- components/Layout.js
+++ components/Layout.js
@@ -3,6 +3,7 @@ import 'tailwindcss/tailwind.css'
 import { Sora } from '@next/font/google'
 import Nav from '../components/Nav'
 import Header from '../components/Header'
+import Head from 'next/head'
 
 const sora = Sora({
   subsets: ['latin'],
@@ -17,6 +18,9 @@ const Layout = ({ children }) => {
   return (
     <div>
       <div className={`page bg-site text-white bg-cover bg-no-repeat ${fontStyles} relative`}>
+        <Head>
+        <meta name='google-site-verification' content='7pY3HSqGDEy_LyhtYnPQmWomQGWQkC740Gmb9Qk6_FI' />
+        </Head>
         <Header />
         <Nav />
         {children}
